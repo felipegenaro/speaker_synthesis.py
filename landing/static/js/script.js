8a392234ab602e17f55b05776dc76845afca160f
Felipe Corniani de Genaro <felipegenaro@outlook.com.br>
2021-02-11 17:23:48 -0300

    text to speech added. among with the sounds settings

--- landing/static/js/script.js
+++ landing/static/js/script.js
@@ -3,42 +3,88 @@ const selector = selector => document.querySelector(selector);
 let recognizing;
 let recognition = new window.webkitSpeechRecognition();
 recognition.continuous = true;
-recognition.lang = 'pt-BR';
+recognition.lang = "pt-BR";
+
+let uploadFile = "";
+let documentReader = new FileReader();
+
+let rate = selector("#rate");
+let rateValue = selector("#rateValue");
+
+let pitch = selector("#pitch");
+let pitchValue = selector("#pitchValue");
+
+let listenRate = 1;
+let listenPitch = 1;
+
+if(selector("#buttonUploadTextFile")) updateFile = selector("#buttonUploadTextFile");
 
 recognition.onresult = (event) => {
         for (let i = event.resultIndex; i < event.results.length; ++i) {
                 if (event.results[i].isFinal) {
-                        textarea.value += event.results[i][0].transcript;
+                        selector("#textarea-stt").value += event.results[i][0].transcript;
                 }
         }
 }
 
 const reset = () => {
         recognizing = false;
-        selector('#buttonSpeech').innerHTML = "Click para Falar";
+        selector("#buttonSpeech").innerHTML = "Click para Falar";
 }
 
 const toggleStartStop = () => {
         if (recognizing) {
                 recognition.stop();
-                reset();
+                
+                if(selector("#buttonSpeech")) reset();
         } else {
                 recognition.start();
                 recognizing = true;
-                selector('#buttonSpeech').innerHTML = "Click para Parar";
-                textarea.value = "";
+                selector("#buttonSpeech").innerHTML = "Click para Parar";
+                selector("#textarea-stt").value = "";
         }
 }
 
-const listen = () => {
+const listen = (txtAreaName) => {
         let hear = new SpeechSynthesisUtterance;
 
         hear.lang = "pt-BR";
-        hear.rate = 1;
-        hear.text = textarea.value;
+        hear.rate = listenRate;
+        hear.pitch = listenPitch;
+        hear.text = selector(`#${txtAreaName}`).value;
 
         speechSynthesis.speak(hear);
 }
 
-reset();
+uploadFile.onchange = () => {
+        let file = selector("#buttonUploadTextFile").files[0];
+        
+        documentReader.readAsText(file);        
+
+        let txtArea = selector("#textarea-tts");
+        txtArea.value = "Aguarde Enquanto seu Arquivo é carregado...";
+}
+
+documentReader.onload = (e) => {
+        let txtArea = selector("#textarea-tts");
+
+        try {
+                txtArea.value = e.target.result;
+        } catch(e) {
+                alert("Somethin went Wrong!" + e.message);
+        } 
+};
+
+rate.onchange = () => {
+        rateValue.innerHTML = `Velocidade ${rate.value}`;
+        listenRate = rate.value;
+}
+
+pitch.onchange = () => {
+        pitchValue.innerHTML = `Tonalidade ${pitch.value}`;
+        listenPitch = pitch.value;
+}
+
+
+if(selector("#buttonSpeech")) reset();
 recognition.onend = reset;
